812d3644c7321d16172db5ac627d5973a235458a
Mateus Oliveira Paula <73196791+MateusPaula@users.noreply.github.com>
2023-11-27 23:51:00 +0000

    add `compose` config file with database service

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,7 @@
 function status(request, response) {
-    response.status(200).json({chave: "valor"})
+  //NÃO VAI TER CHARSET-UTF8
+  //   response.status(200).send("são");
+  response.status(200).json({ chave: "valor" });
 }
 
 export default status;
