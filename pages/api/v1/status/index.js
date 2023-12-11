3e5f585b57fcff960c2c98edaf4e1c5b3abf1ffc
Mateus Oliveira Paula <73196791+MateusPaula@users.noreply.github.com>
2023-12-11 13:43:57 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   //NÃO VAI TER CHARSET-UTF8
   //   response.status(200).send("são");
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
 
