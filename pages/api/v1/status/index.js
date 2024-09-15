47ca3c545c50ce99cd9c5196db13852a870f020b
Mateus Oliveira Paula <73196791+MateusPaula@users.noreply.github.com>
2024-09-15 23:19:05 +0000

    add new fields to the status page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,19 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  //   NÃO VAI TER CHARSET-UTF8
-  //   response.status(200).send("são");
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const versionResult = await database.query("SELECT version()");
+  const connectionsResult = await database.query(
+    "SELECT count(distinct(numbackends)) FROM pg_stat_database"
+  );
+  const maxConnectionsResult = await database.query("SHOW max_connections");
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: versionResult.rows[0].version,
+    max_connections: maxConnectionsResult.rows[0].max_connections,
+    connections: connectionsResult.rows[0].count,
+  });
 }
 
 export default status;
